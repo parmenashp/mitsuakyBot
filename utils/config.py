d5711e77f183414924787ff57d35db57b6491f21
Pármenas Haniel <parmenashp@gmail.com>
2022-07-10 20:44:15 -0300

    Fix config class

--- utils/config.py
+++ utils/config.py
@@ -14,20 +14,29 @@ class BotConfig:
         self.initial_extensions = toml_value.get("initial_extensions", [])
 
 
-class DatabaseConfig:
-    __slots__ = {"username", "password", "name"}
+class KarmaConfig:
+    __slots__ = {"channels"}
 
-    username = os.getenv("POSTGRES_USERNAME")
-    password = os.getenv("POSTGRES_PASSWORD")
-    database_name = os.getenv("POSTGRES_DB_NAME")
+    def __init__(self, toml_value):
+        self.channels = toml_value.get("channels", [])
+
+
+class DatabaseConfig:
+    __slots__ = {"username", "password", "database_name"}
+
+    def __init__(self):
+        self.username = os.getenv("POSTGRES_USERNAME")
+        self.password = os.getenv("POSTGRES_PASSWORD")
+        self.database_name = os.getenv("POSTGRES_DB_NAME")
 
 
 class Config:
-    __slots__ = {"bot", "db"}
+    __slots__ = {"bot", "db", "toml_values"}
 
     def __init__(self, toml_path: str = DEFAULT_FILE_LOCATION) -> None:
         with open(toml_path, "rb") as f:
             self.toml_values = tomli.load(f)
 
-        self.db = DatabaseConfig
+        self.db = DatabaseConfig()
         self.bot = BotConfig(self.toml_values["bot"])
+        self.karma = KarmaConfig(self.toml_values["karma"])
