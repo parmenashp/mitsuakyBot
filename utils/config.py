f6de885db637a59dd9d3ef52116424475b3dd227
Pármenas Haniel <parmenashp@gmail.com>
2022-07-10 23:42:10 -0300

    Add new karma configs and emoji validation

--- utils/config.py
+++ utils/config.py
@@ -1,8 +1,28 @@
 import os
-
+from discord import PartialEmoji
 import tomli
+import re
 
 DEFAULT_FILE_LOCATION = "./config.toml"
+
+# Regex for validate and extract discord custom emoji from a string.
+CUSTOM_EMOJI_REGEX = re.compile(
+    r"<?(?P<animated>a)?:?(?P<name>[A-Za-z0-9\_]+):(?P<id>[0-9]{13,20})>?"
+)
+
+
+def extract_emoji(emoji_str: str, emoji_name: str) -> PartialEmoji:
+    """Extract an emoji from a string, returning a `PartialEmoji` object.
+    If the string is not a valid emoji, then it raises a ValueError."""
+    match = CUSTOM_EMOJI_REGEX.match(emoji_str)
+    if match is not None:
+        groups = match.groupdict()
+        animated = bool(groups["animated"])
+        emoji_id = int(groups["id"])
+        name = groups["name"]
+        return PartialEmoji(name=name, animated=animated, id=emoji_id)
+
+    raise ValueError(f"Invalid config emoji string for emoji {emoji_name}")
 
 
 class BotConfig:
@@ -15,10 +35,16 @@ class BotConfig:
 
 
 class KarmaConfig:
-    __slots__ = {"channels"}
+    __slots__ = {"channels", "upvote_emoji", "downvote_emoji"}
 
     def __init__(self, toml_value):
         self.channels = toml_value.get("channels", [])
+        self.upvote_emoji = extract_emoji(
+            toml_value.get("upvote_emoji"), "upvote_emoji"
+        )
+        self.downvote_emoji = extract_emoji(
+            toml_value.get("downvote_emoji"), "downvote_emoji"
+        )
 
 
 class DatabaseConfig:
