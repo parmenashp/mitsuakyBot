4d5b46e69ff8a3595a64deaab3d41df4100f8880
Pármenas Haniel <parmenashp@gmail.com>
2022-07-11 01:15:36 -0300

    Add get_or_fetch for member/guild

--- bot.py
+++ bot.py
@@ -40,6 +40,69 @@ class MitBot(commands.Bot):
             enable_debug_events=True,
         )
 
+    async def get_or_fetch_guild(self, guild_id: int) -> discord.Guild | None:
+        """Looks up a guild in cache or fetches if not found.
+        Parameters
+        -----------
+        guild_id: int
+            The guild ID to search for.
+        Returns
+        ---------
+        Optional[Guild]
+            The guild or None if not found.
+        """
+        guild = self.get_guild(guild_id)
+        if guild is not None:
+            return guild
+
+        if not self.is_ws_ratelimited():
+            try:
+                guild = await self.fetch_guild(guild_id)
+            except discord.HTTPException:
+                return None
+            else:
+                return guild
+
+    async def get_or_fetch_member(
+        self, guild: discord.Guild | int, member_id: int
+    ) -> discord.Member | None:
+        """Looks up a member in cache or fetches if not found.
+        Parameters
+        -----------
+        guild: Guild
+            The guild to look in.
+        member_id: int
+            The member ID to search for.
+        Returns
+        ---------
+        Optional[Member]
+            The member or None if not found.
+        """
+        # Tries to get the guild if only guild id was passed
+        if isinstance(guild, int):
+            _guild = await self.get_or_fetch_guild(guild)
+            if _guild is None:
+                return None
+            guild = _guild
+
+        member = guild.get_member(member_id)
+        if member is not None:
+            return member
+
+        if self.is_ws_ratelimited():
+            try:
+                # Get member from api
+                member = await guild.fetch_member(member_id)
+            except discord.HTTPException:
+                return None
+            else:
+                return member
+        # Get member from websocket
+        members = await guild.query_members(limit=1, user_ids=[member_id], cache=True)
+        if not members:
+            return None
+        return members[0]
+
     async def setup_hook(self):
         for extension in self.config.bot.initial_extensions:
             try:
